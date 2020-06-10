d6eb5c4107cb9800d7557517350d9be9d30a4af2
abdul15821 <50765164+abdul15821@users.noreply.github.com>
2020-06-09 22:31:52 -0500

    Add files via upload

---
+++ CSharpBoxUnbox/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxUnbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+            int sum = 0;
+            List<object> items = new List<object>();
+            items.Add(7);
+            items.Add(28);
+            items.Add(-1);
+            items.Add(true);
+            items.Add("chair");
+            for(var i = 0; i < items.Count; i++)
+            {
+                Console.WriteLine(items[i]);
+            }
+            foreach(var item in items)
+            {
+                if(item is int)
+                {
+                    sum += (int)item;
+                }
+            }
+            Console.WriteLine("Sum: " + sum);
+        }
+        }
+    }
+}
