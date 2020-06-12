2b82eb4dd11c7399ad54d7b537557f9356d08644
Abdul Adewole <50765164+abdul15821@users.noreply.github.com>
2020-06-11 19:08:01 -0500

    Add files via upload

---
+++ CSharpIronNinja/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hungry_Ninja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Buffet yummy= new Buffet();
+            SweetTooth kyle = new SweetTooth();
+            SpiceHound dude = new SpiceHound();
+            while (!kyle.IsFull)
+            {
+                kyle.Consume(yummy.Serve());
+            }
+            if (kyle.ConsumptionHistory.Count > dude.ConsumptionHistory.Count)
+            {
+                Console.WriteLine($"SweetTooth had the most items consumed with {kyle.ConsumptionHistory.Count} items!");
+            }
+            else
+            {
+                Console.WriteLine($"SweetTooth had the most items consumed with {dude.ConsumptionHistory.Count} items!");
+            }
+        }
+    }
+}
