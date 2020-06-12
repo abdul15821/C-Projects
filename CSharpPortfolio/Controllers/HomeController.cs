4b3579ae569388ffa1dd0dfc3bd6c093289d3d66
Abdul Adewole <50765164+abdul15821@users.noreply.github.com>
2020-06-11 20:43:54 -0500

    Add files via upload

---
+++ CSharpPortfolio/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CSharpPortfolio.Models;
+
+namespace CSharpPortfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet("projects")]
+        public IActionResult Projects()
+        {
+            return View();
+        }
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
