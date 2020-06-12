00676058c7ea9985bf1957c4bdc857804a0b8d4c
Abdul Adewole <50765164+abdul15821@users.noreply.github.com>
2020-06-11 21:25:04 -0500

    Add files via upload

---
+++ CSharpDojoSurvey/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CSharpDojoSurvey.Models;
+
+namespace CSharpDojoSurvey.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost("result")]
+        public IActionResult Result(string Name, string Location, string Language, string Comment)
+        {
+            ViewBag.Name = Name;
+            ViewBag.Location = Location;
+            ViewBag.Language = Language;
+            ViewBag.Comment = Comment;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
