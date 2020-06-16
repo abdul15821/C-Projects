6dd7d1de6a0b75b076da2b01468b3c5b55b84568
Abdul Adewole <50765164+abdul15821@users.noreply.github.com>
2020-06-15 19:26:02 -0500

    Add files via upload

---
+++ CSharpViewModelFun/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ViewModelFun.Models;
+
+namespace ViewModelFun.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            ViewBag.Message = "This is a string";
+
+            return View();
+        }
+
+        [HttpGet]
+        [Route("/numbers")]
+        public IActionResult Number()
+        {
+            Random rand = new Random();
+            int[] arrNum = new int[10];
+            for(int i = 0; i < 10; i++)
+            {
+                arrNum[i] = rand.Next();
+                
+            }
+            ViewBag.Nums = arrNum;
+            return View();
+        }
+
+        [HttpGet]
+        [Route("/user")]
+        public IActionResult Person()
+        {
+            Person dude = new Person
+            {
+                fname = "Joey",
+                lname = "Weber"
+            };
+            ViewBag.User = dude;
+            return View("Person", dude);
+        }
+
+        [HttpGet]
+        [Route("/users")]
+        public IActionResult People()
+        {
+            Console.WriteLine("***********************************************");
+            Console.WriteLine("In the people route");
+            Console.WriteLine("***********************************************");
+            Person Joey = new Person
+            {
+                fname = "Joey",
+                lname = "Weber"
+            };
+            Person Nicole = new Person
+            {
+                fname = "Nicole",
+                lname = "Weber"
+            };
+            Person Chris = new Person
+            {
+                fname = "Chris",
+                lname = "Jager"
+            };
+            List<Person> people = new List<Person>()
+            {
+                Joey, Nicole, Chris
+            };
+            ViewBag.User = people;
+            return View("People", people);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel{ RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CSharpViewModelFun/Models/Random.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace ViewModelFun.Models
+{
+    public class Person
+    {
+        public string fname{get;set;} 
+        public string lname{get;set;}
+    }
+    
+}
