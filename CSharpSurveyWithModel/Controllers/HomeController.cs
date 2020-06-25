00dd319b633b5c4e90cf6f03c7755d6a8ee6b634
Abdul Adewole <50765164+abdul15821@users.noreply.github.com>
2020-06-25 12:21:46 -0500

    Add files via upload

---
+++ CSharpSurveyWithModel/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CSharpSurveyWithModel.Models;
+
+namespace CSharpSurveyWithModel.Controllers
+{
+    public class HomeController : Controller
+   {
+
+        [HttpGet("")]
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        [HttpPost("result")]
+        public ActionResult Result(string name, string location, string language, string textarea)
+        {
+            ViewBag.name = name;
+            ViewBag.location = location;
+            ViewBag.language = language;
+            ViewBag.textarea = textarea;
+
+            return View();
+        }
+
+        //Redirect back to Index if trying to access result page through GET
+        [HttpGet("result")]
+        public RedirectToActionResult Redirect()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
